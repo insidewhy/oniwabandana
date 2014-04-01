3ec84aad183af300df788414ed32de59be5ef799
James Pike <jpike@chilon.net>
2014-04-01 01:30:19 +0100

    Improvements/fixes to match scoring + more test.

--- plugin/match.rb
+++ plugin/match.rb
@@ -1,8 +1,9 @@
 module Oniwabandana
-  CriterionMatch = Struct.new :score, :idx, :multiplier
+  CriterionIndex = Struct.new :index, :multiplier
+  CriterionMatch = Struct.new :score, :indexes
 
   class Match
-    attr_reader :filename, :score
+    attr_reader :filename, :score, :matches
 
     def initialize filename
       @filename = filename
@@ -16,36 +17,84 @@ module Oniwabandana
     # Params:
     # +criteria+:: Array of strings to apply as criteria.
     def increase_score! criteria
-      if criteria.last.size == 1
+      crit_size = criteria.last.size
+      if crit_size == 1
         # the last criterion is new
-        offset = @matches.empty? ? 0 : @matches.last.idx + 1
+        offset = @matches.empty? ? 0 : @matches.last.indexes.first.index + 1
         idx = @filename.index criteria.last, offset
         if idx.nil?
           @score = -1
         else
           multiplier = 1
-          multiplier *= 2 if idx == 0 || '._/ '.index(@filename[idx - 1])
-          multiplier *= 2 if idx >= @file_idx
-          @matches << CriterionMatch.new(multiplier * 10, idx, multiplier)
-          @score += @matches.last.score
+          calc_multiplier = proc do
+            multiplier = 1
+            multiplier *= 2 if idx == 0 || '._/ '.index(@filename[idx - 1])
+            multiplier *= 2 if idx >= @file_idx
+          end
+          calc_multiplier.call
+
+          match = CriterionMatch.new(multiplier, [CriterionIndex.new(idx, multiplier)])
+          @matches << match
+          # search for the criterion multiple times
+          while true
+            idx = @filename.index criteria.last, idx + 1
+            # todo: recalculate multiplier
+            break if idx.nil?
+            calc_multiplier.call
+            match.indexes << CriterionIndex.new(idx, multiplier)
+            match.score += multiplier
+          end
+
+          @score += match.score
         end
       else
         # the last criterion was updated
-        offset = @matches.size > 1 ? @matches[-2].idx + 1 : 0
-        idx = @filename.index criteria.last, offset
-        if idx.nil?
+        match = @matches.last
+        @score -= match.score
+        match.score = 0
+
+        prev_idx = index_idx = -1
+        # use previous set of indexes as a guide to find next indexes
+        fail_idx = match.indexes.index do |idx|
+          if idx.index < prev_idx
+            false
+          elsif idx.index == prev_idx
+            # catchup from previous iteration
+            match.indexes[index_idx += 1].multiplier = idx.multiplier
+            match.indexes[index_idx].index = prev_idx
+            match.score += idx.multiplier * crit_size
+            false
+          else
+            index = @filename.index criteria.last, idx.index
+            if index.nil?
+              true
+            else
+              # make sure the correct multiplier is used
+              if index == idx.index
+                match.indexes[index_idx += 1].index = index
+                match.score += idx.multiplier * crit_size
+              end
+              prev_idx = index
+              false
+            end
+          end
+        end
+
+        if match.indexes.size >= index_idx + 1
+          match.indexes = match.indexes[0..index_idx]
+        end
+
+        if fail_idx == 0
           @score = -1
         else
-          last_match = @matches.last
-          @score += 10 * last_match.multiplier
-          last_match.idx = idx
-          last_match.score += 10 * last_match.multiplier
+          @score += match.score
         end
       end
     end
 
+    # reverse order as higher score should be ranked first
     def <=> rhs
-      @score <=> rhs.score
+      rhs.score <=> @score
     end
 
     def matching?
--- plugin/spec/match_spec.rb
+++ plugin/spec/match_spec.rb
@@ -5,9 +5,9 @@ module Oniwabandana
     it 'with update to single criterion in middle' do
       m = Match.new 'thefile'
       m.increase_score! ['f']
-      expect(m.score).to eq(20)
+      expect(m.score).to eq(2)
       m.increase_score! ['fi']
-      expect(m.score).to eq(40)
+      expect(m.score).to eq(4)
     end
 
     it 'fails single criterion' do
@@ -19,24 +19,24 @@ module Oniwabandana
     it 'and update single criterion at beginning with multiplier' do
       m = Match.new 'thefile'
       m.increase_score! ['t']
-      expect(m.score).to eq(40)
+      expect(m.score).to eq(4)
       m.increase_score! ['th']
-      expect(m.score).to eq(80)
+      expect(m.score).to eq(8)
     end
 
     it 'after undescore in middle with multiplier' do
       m = Match.new 'the_file'
       m.increase_score! ['f']
-      expect(m.score).to eq(40)
+      expect(m.score).to eq(4)
       m.increase_score! ['fi']
-      expect(m.score).to eq(80)
+      expect(m.score).to eq(8)
     end
 
     it 'failing second criterion' do
       m = Match.new 'thefile'
       m.increase_score! ['t']
       m.increase_score! ['th']
-      expect(m.score).to eq(80)
+      expect(m.score).to eq(8)
       m.increase_score! ['th', 'b']
       expect(m.score).to eq(-1)
     end
@@ -44,13 +44,13 @@ module Oniwabandana
     it 'weighted criterion followed by unweighted criterion' do
       m = Match.new 'thefile'
       m.increase_score! ['t']
-      expect(m.score).to eq(40)
+      expect(m.score).to eq(4)
       m.increase_score! ['th']
-      expect(m.score).to eq(80)
+      expect(m.score).to eq(8)
       m.increase_score! ['th', 'f']
-      expect(m.score).to eq(100)
+      expect(m.score).to eq(10)
       m.increase_score! ['th', 'fi']
-      expect(m.score).to eq(120)
+      expect(m.score).to eq(12)
       m.increase_score! ['th', 'fio']
       expect(m.score).to eq(-1)
     end
@@ -58,20 +58,20 @@ module Oniwabandana
     it 'with lower multiplier against directory' do
       m = Match.new 'dir/file'
       m.increase_score! ['d']
-      expect(m.score).to eq(20)
+      expect(m.score).to eq(2)
       m.increase_score! ['di']
-      expect(m.score).to eq(40)
+      expect(m.score).to eq(4)
       m.increase_score! ['di', 'f']
-      expect(m.score).to eq(80)
+      expect(m.score).to eq(8)
       m.increase_score! ['di', 'fi']
-      expect(m.score).to eq(120)
+      expect(m.score).to eq(12)
     end
 
     it 'ensures second criterion must be after first' do
       m = Match.new 'abc_def_ghi'
       m.increase_score! ['d']
       m.increase_score! ['de']
-      expect(m.score).to eq(80)
+      expect(m.score).to eq(8)
       m.increase_score! ['de', 'a']
       expect(m.score).to eq(-1)
     end
@@ -80,9 +80,53 @@ module Oniwabandana
       m = Match.new 'den_bon'
       m.increase_score! ['d']
       m.increase_score! ['de']
-      expect(m.score).to eq(80)
+      expect(m.score).to eq(8)
       m.increase_score! ['de', 'd']
       expect(m.score).to eq(-1)
+    end
+
+    it 'a single criterion multiple times' do
+      m = Match.new 'ban ban'
+      m.increase_score! ['b']
+      expect(m.score).to eq(8) # 4 from each match
+    end
+
+    it 'a single criterion multiple times with an update' do
+      m = Match.new 'ban ban'
+      m.increase_score! ['b'] # score == 8
+      m.increase_score! ['ba']
+      expect(m.score).to eq(16) # 8 from each match
+    end
+
+    it 'matches a criterion multiple times with updates' do
+      m = Match.new 'ban don sban sdon'
+      m.increase_score! ['b']
+      expect(m.score).to eq(6) # 4 + 2
+    end
+
+    it 'removes the score of a criterion that matches twice then once' do
+      m = Match.new 'baan0baon'
+      m.increase_score! ['b'] # 4 + 2
+      m.increase_score! ['ba'] # (4 * 2) + (2 * 2)
+      expect(m.score).to eq(12)
+      m.increase_score! ['baa'] # (4 * 3)
+      expect(m.score).to eq(12)
+    end
+
+    it 'passes a real-world example' do
+      m = Match.new 'app/controller/application_controller.rb'
+      m.increase_score! ['a'] # 2 + 4 + 2
+      expect(m.score).to eq(8)
+      m.increase_score! ['ap'] # (2 * 2)  + (2 * 4)
+      expect(m.score).to eq(12)
+      m.increase_score! ['app'] # (3 * 2)  + (3 * 4)
+      expect(m.score).to eq(18)
+      m.increase_score! ['app', 'c'] # 18 + 2 + 2 + 4
+      expect(m.score).to eq(26)
+      m.increase_score! ['app', 'co'] # 18 + (2 * 2) + (2 * 4)
+      expect(m.score).to eq(30)
+      m.increase_score! ['app', 'con'] # 18 + (3 * 2) + (3 * 4)
+      expect(m.score).to eq(36)
     end
   end
 end
